72d5a55751c8fd5279a5993d515bdc06511a9eb7
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-02-13 16:39:47 +0000

    Validate HTML against W3C validator; fix html indenting

--- src/page-template.js
+++ src/page-template.js
@@ -3,59 +3,56 @@ const generateTeam = team => {
 
     // creates the manager html
     const generateManager = manager => {
-        return `
-    <div class="card employee-card shadow mb-4">
-        <div class="card-header">
-            <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-            </ul>
-        </div>
-    </div>
-        `;
+        return `<div class="card employee-card shadow mb-4">
+                        <div class="card-header">
+                            <h2 class="card-title">${manager.getName()}</h2>
+                            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item">ID: ${manager.getId()}</li>
+                                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+                            </ul>
+                        </div>
+                    </div>
+                    `;
     };
 
     // creates the html for engineers
     const generateEngineer = engineer => {
-        return `
-<div class="card employee-card shadow mb-4">
-    <div class="card-header">
-        <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${engineer.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
-        </ul>
-    </div>
-</div>
-        `;
+        return `<div class="card employee-card shadow mb-4">
+                        <div class="card-header">
+                            <h2 class="card-title">${engineer.getName()}</h2>
+                            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item">ID: ${engineer.getId()}</li>
+                                <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                                <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    `;
     };
 
     // creates the html for interns
     const generateIntern = intern => {
-        return `
-<div class="card employee-card shadow mb-4">
-    <div class="card-header">
-        <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${intern.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-            <li class="list-group-item">School: ${intern.getSchool()}</li>
-        </ul>
-    </div>
-</div>
-        `;
+        return `<div class="card employee-card shadow mb-4">
+                        <div class="card-header">
+                            <h2 class="card-title">${intern.getName()}</h2>
+                            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item">ID: ${intern.getId()}</li>
+                                <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                                <li class="list-group-item">School: ${intern.getSchool()}</li>
+                            </ul>
+                        </div>
+                    </div>
+                    `;
     };
 
     const html = [];
@@ -82,38 +79,36 @@ const generateTeam = team => {
 // exports function to generate entire page
 module.exports = team => {
 
-    return `
-    <!DOCTYPE html>
+    return `<!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/c502137733.js"></script>
-</head>
-
-<body>
-    <div class="jumbotron jumbotron-fluid">
-        <div class="container-fluid">
-            <div class="mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="team-area col-12 d-flex flex-wrap justify-content-center">
-                <div class="card-deck justify-content-center">
-                    ${generateTeam(team)}
+    <body>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container-fluid">
+                <div class="mb-3 team-heading">
+                    <h1 class="text-center">My Team</h1>
                 </div>
             </div>
         </div>
-    </div>
-</body>
+        <div class="container">
+            <div class="row">
+                <div class="team-area col-12 d-flex flex-wrap justify-content-center">
+                    <div class="card-deck justify-content-center">
+                        ${generateTeam(team)}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </body>
 </html>
     `;
 };
